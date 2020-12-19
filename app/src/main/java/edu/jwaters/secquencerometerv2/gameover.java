9b739e4df86fc9db3bcd3c4fc28e447d4065ed4b
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2020-12-19 16:48:56 +0000

    screen fixed but must remove the watch settings

--- app/src/main/java/edu/jwaters/secquencerometerv2/gameover.java
+++ app/src/main/java/edu/jwaters/secquencerometerv2/gameover.java
@@ -1,22 +1,22 @@
 package edu.jwaters.secquencerometerv2;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class play_screen extends AppCompatActivity {
+public class gameover extends AppCompatActivity {
 
     private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_screen);
+        setContentView(R.layout.activity_gameover);
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        // Enables Always-on
+        
     }
-
 }
